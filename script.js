7eabcc3d1f76dde603a82730cb8f11e8ae7b0a2d
dgcezar <diego.cezar13@gmail.com>
2022-10-03 12:46:29 -0300

    Primeira tentativa

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 const frutas = ["laranja", "limão", "uva"];
+
+const fruta1 = document.getElementById("fruta-1");
+fruta1.innerHTML = frutas[0]
+const fruta2 = document.getElementById("fruta-2");
+fruta2.innerHTML = frutas[1]
+const fruta3 = document.getElementById("fruta-3");
+fruta3.innerHTML = frutas[2]
+
+const frutaInput = document.getElementById("fruta").value;
+
+const adicionarFruta = () => {
+    frutas.push(frutaInput);
+    const fruta4 = document.getElementById("fruta-4");
+    fruta4.innerHTML = frutas[3]
+}
